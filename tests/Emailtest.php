1a664a687d2879b7ec4d01512852bcd142ad05df
alma31 <lenni31@neuf.fr>
2017-01-26 11:11:27 +0100

    phpunit 1er test ok

---
+++ tests/Emailtest.php
@@ -0,0 +1,30 @@
+<?php 
+declare(strict_types=1);
+use PHPUnit\Framework\TestCase;
+
+final class EmailTest extends TestCase{
+	public function testCanBeCreatedFromValidEmailAdress() {
+
+		$this->assertInstanceOf(
+			Email::class,
+			Email::fromString('user@example.com')
+
+			);
+
+
+
+	}
+
+	public function testCannotBeCreatedFromValidEmailAdress() {
+
+		$this->expectException(InvalidArgumentException::class);
+		Email::fromString('Invalid');
+	}
+	public function testCanBeUsedAsString() {
+		$this->assertEquals('user@example.com',
+			Email::fromString('user@example.com')
+			);
+	}
+}
+
+?>
