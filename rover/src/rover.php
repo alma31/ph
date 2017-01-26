1569cd660dafcb45ca65184818f5c62833933d82
alma31 <lenni31@neuf.fr>
2017-01-26 11:31:16 +0100

    rover init ok

---
+++ rover/src/rover.php
@@ -0,0 +1,25 @@
+<?php 
+
+class Rover {
+
+	private $x;
+	private $y;
+
+	function __construct($x,$y){
+		$this->x;
+		$this->y;
+	}
+
+	public function getX(){
+		$this->$x = x;
+	}
+
+	public function getY(){
+		$this->$y = y;
+	}
+
+
+
+}
+
+?>
